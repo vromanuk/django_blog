27f1b3f5a197370914f692bc6c67297f6f5ef17a
Romaniuk Vlad <romanuk_vlad@ukr.net>
2020-05-14 13:46:49 +0300

    add bootstrap to the project

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html')
