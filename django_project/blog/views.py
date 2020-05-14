749d4f536207f0b46807ef4f6375a8f675ae509c
Romaniuk Vlad <romanuk_vlad@ukr.net>
2020-05-14 14:24:49 +0300

    create post model

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 
+from .models import Post
+
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
